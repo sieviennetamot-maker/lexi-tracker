141501775147eba5772851a78be8ccaff8506285
sieviennetamot-maker <sieviennetamot@gmail.com>
2025-08-19 14:54:36 -0500

    sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Simple service worker for Lexi's Budget Tracker
+
+const CACHE_NAME = "lexi-v1";
+const ASSETS = ["./", "./index.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
